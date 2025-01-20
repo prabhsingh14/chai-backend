9f915d80bb3835443bcc7fd83ab8f82653ee1eaa
prabhsingh14 <psingh10_be21@thapar.edu>
2025-01-20 22:40:47 +0530

    Error message overwritten issue resolved

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,24 +1,23 @@
 class ApiError extends Error {
     constructor(
-        statusCode,
-        message= "Something went wrong",
-        errors = [],
-        stack = ""
-    ){
-        super(message)
-        this.statusCode = statusCode
-        this.data = null
-        this.message = message
-        this.success = false;
-        this.errors = errors
+    statusCode,
+    message = "Something went wrong",
+    errors = [],
+    stack = ""
+) {
+    super(); // Do not pass the `message` to the base Error class, to avoid it doen't get overwritten by the base Error.
+    this.statusCode = statusCode;
+    this.data = null;
+    this.message = message; // Explicitly set the message property.
+    this.success = false;
+    this.errors = errors;
 
-        if (stack) {
-            this.stack = stack
-        } else{
-            Error.captureStackTrace(this, this.constructor)
-        }
-
+    if (stack) {
+    this.stack = stack;
+    } else {
+    Error.captureStackTrace(this, this.constructor);
     }
 }
+}
 
-export {ApiError}
+export default ApiError;
