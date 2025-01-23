6f26ca7f164a47c7d9f7226eea842bc53109846e
Prabh Singh <101253649+prabhsingh14@users.noreply.github.com>
2025-01-23 12:57:38 +0530

    Update comment.controller.js

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -5,27 +5,93 @@ import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 const getVideoComments = asyncHandler(async (req, res) => {
-    //TODO: get all comments for a video
-    const {videoId} = req.params
-    const {page = 1, limit = 10} = req.query
-
+    try{
+        const {videoId} = req.params
+        const {page = 1, limit = 10} = req.query
+    
+        const comments = await Comment.find({videoId}).sort({createdAt:-1})
+    
+        next(new ApiResponse(200, comments))
+    } catch(error){
+        next(new ApiError(500, error.message))
+    }
 })
 
 const addComment = asyncHandler(async (req, res) => {
-    // TODO: add a comment to a video
+    const {videoId, text} = req.body
+
+    if(!videoId || !text){
+        next(new ApiError(400, "Please provide all details"))
+    }
+
+    try{
+        const newComment = await Comment.create({
+            video: videoId,
+            content: text,
+            owner: req.user._id
+        }).save();
+
+        next(new ApiResponse(201, newComment))
+    } catch(error){
+        next(new ApiError(500, error.message))
+    }
 })
 
 const updateComment = asyncHandler(async (req, res) => {
-    // TODO: update a comment
+    const {commentId, text} = req.body;
+
+    if(!commentId || !text){
+        next(new ApiError(400, "Please provide all details"))
+    }
+
+    try{
+        const comment = await Comment.findById(commentId);
+
+        if(!comment){
+            next(new ApiError(404, "Comment not found"))
+        }
+
+        if(comment.owner.toString() !== req.user._id.toString()){
+            next(new ApiError(403, "You are not authorized to update this comment"))
+        }
+
+        comment.content = text;
+        
+        const updatedComment = await comment.save();
+
+        next(new ApiResponse(200, updatedComment))
+    } catch(error){
+        next(new ApiError(500, error.message))
+    }
 })
 
 const deleteComment = asyncHandler(async (req, res) => {
-    // TODO: delete a comment
+    const {commentId} = req.body;
+    if(!commentId){
+        next(new ApiError(400, "Please provide comment id"))
+    }
+
+    const comment = await Comment.findById(commentId);
+    if(!comment){
+        next(new ApiError(404, "Comment not found"))
+    }
+
+    try{
+        if(comment.owner.toString() !== req.user._id.toString()){
+            next(new ApiError(403, "You are not authorized to delete this comment"))
+        }
+
+        await comment.remove();
+
+        next(new ApiResponse(200, "Comment deleted successfully"))
+    } catch(error){
+        next(new ApiError(500, error.message))
+    }
 })
 
 export {
     getVideoComments, 
     addComment, 
     updateComment,
-     deleteComment
-    }
+    deleteComment
+}
